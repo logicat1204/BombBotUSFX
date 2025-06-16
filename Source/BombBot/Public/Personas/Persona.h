6d8b7df664778261990ec288ee79257e22a6ea1e
Gabrielo1212 <gabodrec2@gmail.com>
2025-06-16 08:48:53 -0400

    Edicion en persona y agregados meshes animales
    Se agrego todos los meshes con sus animaciones y se hizo cambios en persona para que elija un animal al azar para mostrar

--- Source/BombBot/Public/Personas/Persona.h
+++ Source/BombBot/Public/Personas/Persona.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Power_Ups/PowerUp.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/RotatingMovementComponent.h"
 #include "Persona.generated.h"
 
 /**
@@ -17,14 +19,44 @@ class BOMBBOT_API APersona : public APowerUp
 public:
 
 	APersona();
+    
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    USkeletalMeshComponent* MiSkeletalMesh;
+    
+    // Usamos TSoftObjectPtr para las referencias a assets que se cargarán en tiempo de ejecución.
+    // Esto permite que Blueprint también las asigne.
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Assets")
+    TSoftObjectPtr<USkeletalMesh> FoxSkeletalMeshAsset;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Assets")
+    TSoftObjectPtr<UAnimSequence> FoxAnimAsset;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Assets")
+    TSoftObjectPtr<USkeletalMesh> WolfSkeletalMeshAsset;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Assets")
+    TSoftObjectPtr<UAnimSequence> WolfAnimAsset;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Assets")
+    TSoftObjectPtr<USkeletalMesh> PigSkeletalMeshAsset;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Assets")
+    TSoftObjectPtr<UAnimSequence> PigAnimAsset;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Assets")
+    TSoftObjectPtr<USkeletalMesh> DeerDoeSkeletalMeshAsset;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Assets")
+    TSoftObjectPtr<UAnimSequence> DeerDoeAnimAsset;
+
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+    UAnimSequence* CurrentAnimSequence;
+    
+    virtual void BeginPlay() override;
 
 protected:
 
 	virtual bool ActivatePowerUp(class ABombBotCharacter* PlayerCharacter) override;
 
-
-private:
-
-	UPROPERTY(EditAnywhere)
-	UStaticMeshComponent* MallaPowerUp;
 };
