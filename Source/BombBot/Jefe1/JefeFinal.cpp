fcd4ec4be23c519c9ce01e6c19d1c92681684548
Alvaro David Arancibia Estrada <aarancibia142@gmail.com>
2025-06-15 22:36:03 -0400

    Update JefeFinal.cpp

--- Source/BombBot/Jefe1/JefeFinal.cpp
+++ Source/BombBot/Jefe1/JefeFinal.cpp
@@ -9,7 +9,7 @@ AJefeFinal::AJefeFinal()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	MeshJefe = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MallaJefe"));
-	RootComponent = MeshJefe;
+	MeshJefe->SetupAttachment(RootComponent);
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh> EsqueletoJefe(TEXT("/Script/Engine.SkeletalMesh'/Game/AlvaroAssets/FinalBoss/tentacle-robot/source/Robot_export2.Robot_export2'"));
 	if (EsqueletoJefe.Succeeded())
 	{
@@ -17,10 +17,15 @@ AJefeFinal::AJefeFinal()
 		MeshJefe->SetRelativeLocation(FVector(0.0f,0.0f,0.0f));
 		MeshJefe->SetRelativeRotation(FRotator(0.0f, 0.0f, 0.0f));
 	}
-	static ConstructorHelpers::FObjectFinder<UMaterial> MaterialJefe(TEXT("/Script/Engine.Material'/Game/AlvaroAssets/FinalBoss/tentacle-robot/source/Material_010.Material_010'"));
+	static ConstructorHelpers::FObjectFinder<UMaterial> MaterialJefe(TEXT("/Script/Engine.Material'/Game/AlvaroAssets/FinalBoss/tentacle-robot/textures/Material_011_BaseColor_Mat.Material_011_BaseColor_Mat'"));
 	if (MaterialJefe.Succeeded())
 	{
 		MeshJefe->SetMaterial(0, MaterialJefe.Object);
+	}
+	static ConstructorHelpers::FObjectFinder<UMaterial> MaterialJefe2(TEXT("/Script/Engine.Material'/Game/AlvaroAssets/FinalBoss/tentacle-robot/textures/Material_011_BaseColor_Mat.Material_011_BaseColor_Mat'"));
+	if (MaterialJefe2.Succeeded())
+	{
+		MeshJefe->SetMaterial(1, MaterialJefe2.Object);
 	}
 	static ConstructorHelpers::FObjectFinder<UAnimationAsset> AnimJefe(TEXT("/Script/Engine.AnimSequence'/Game/AlvaroAssets/FinalBoss/tentacle-robot/source/Robot_export2_Anim.Robot_export2_Anim'"));
 	if (AnimJefe.Succeeded())
